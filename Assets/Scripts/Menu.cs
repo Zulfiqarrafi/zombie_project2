96015ef1ffe25b5da295b80b666fe6be8c606740
Zulfiqarrafi <rafijust89@gmail.com>
2024-12-12 23:33:50 +0700

    first commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Pindah ke Scene Menu
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    // Pindah ke Scene Asylum
+    public void LoadAsylumScene()
+    {
+        SceneManager.LoadScene("Asylum");
+    }
+
+    // Keluar dari Game
+    public void QuitGame()
+    {
+        Debug.Log("Game Quit!");
+        Application.Quit();
+    }
+}
